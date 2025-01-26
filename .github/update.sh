2b9d8ae7d91a2a68aa7590dabd7bba7d03927056
Foez Ahmed <foez.official@gmail.com>
2025-01-26 22:59:18 +0600

    new file:   .github/update.sh new file:   .github/workflows/update_all.yml modified:   README.md

---
+++ .github/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r *.md
+
+echo "# VLSI Training" > README.md
+
+LIST=$(find -mindepth 1 -maxdepth 1 -type d ! -name ".*" | sed "s/.*\///g")
+for i in ${LIST} ; do
+    echo "  - [$(echo ${i} | sed -e 's/_/ /g' -e 's/\b\(.\)/\u\1/g')](${i}.md)" >> README.md
+    echo "# $(echo ${i} | sed -e 's/_/ /g' -e 's/\b\(.\)/\u\1/g')" > ${i}.md
+    LIST2=$(find ${i} -mindepth 1 -maxdepth 1 -type f -name "chapter_*.md")
+    for j in ${LIST2} ; do
+        TITLE=$(cat ${j} | grep -m 1 -oP "(?<=# ).*")
+        echo "## $(echo "${j}" | sed "s/.*chapter_0*//g" | sed "s/\..*//g"). [$(echo ${TITLE} | sed -e 's/_/ /g' -e 's/\b\(.\)/\u\1/g')](${j})" >> ${i}.md
+        cat ${j} | grep "^## " | sed "s/^## /  - /g" >> ${i}.md
+    done
+    echo "" >> ${i}.md
+done
+
+echo "" >> README.md
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# training
+# VLSI Training
+  - [Digital Training](digital_training.md)
+
